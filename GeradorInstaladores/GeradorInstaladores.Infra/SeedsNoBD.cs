9d1f5b4d248d4254e0ee03cb2de2d63346175cd5
Gustavo Breunig <gustavofbreunig@gmail.com>
2018-05-23 13:20:06 -0300

    teste sqlite e definicoes do INNO

---
+++ GeradorInstaladores/GeradorInstaladores.Infra/SeedsNoBD.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorInstaladores.Infra
+{
+    public class SeedsNoBD
+    {
+        public static void FazSeedSQLite()
+        {
+            //faz o seed dos modelos
+            using (var db = new GeradorInstaladoresContext())
+            {
+                int modelosCadastrados = db.ModelosEquipamentos.Count();
+
+                if (modelosCadastrados == 0)
+                {
+
+                    db.ModelosEquipamentos.Add(new ModeloEquipamento()
+                    {
+                        NomeModelo = "RICOH Aficio MP C2550",
+                        PastaDriverX86 = "mpc2050_x86",
+                        PastaDriverX64 = "mpc2050_x64",
+                        ArquivoINF = "OEMSETUP.INF",
+                        NomeDriver = "RICOH Aficio MP C2550 PCL 5c"
+                    });
+
+                    db.ModelosEquipamentos.Add(new ModeloEquipamento()
+                    {
+                        NomeModelo = "Ricoh MP 305",
+                        PastaDriverX64 = "mp305_x64",
+                        PastaDriverX86 = "mp305_x86",
+                        ArquivoINF = "OEMSETUP.INF",
+                        NomeDriver = "RICOH MP 305+ PCL 5e"
+                    });
+
+                    db.ModelosEquipamentos.Add(new ModeloEquipamento()
+                    {
+                        NomeModelo = "RICOH Aficio MP 201",
+                        PastaDriverX64 = "mp201_x64",
+                        PastaDriverX86 = "mp201_x86",
+                        ArquivoINF = "OEMSETUP.INF",
+                        NomeDriver = "RICOH Aficio MP 201 PCL 5e"
+                    });
+                }
+
+                int definicoesGravadas = db.DefinicoesGerais.Count();
+
+                if (definicoesGravadas == 0)
+                {
+                    db.DefinicoesGerais.Add(new DefinicoesGerais()
+                    {
+                        PastaDrivers = @"D:\temp\instaladorImpressoras",
+                        PastaINNO = @"C:\Program Files (x86)\Inno Setup 5",
+                        AppName = @"AppName",
+                        AppPublisher = @"AppPublisher",
+                        AppPublisherURL = @"http://google.com",
+                        AppSupportURL = @"http://google.com",
+                        AppUpdatesURL = @"http://google.com"
+                    });
+                }
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ GeradorInstaladores/GeradorInstaladores.Testes/TesteSQLite.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GeradorInstaladores.Infra;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeradorInstaladores.Testes
+{
+    [TestClass]
+    public class TesteSQLite
+    {
+        [TestMethod]
+        public void testa_seed_sqlite()
+        {
+            SeedsNoBD.FazSeedSQLite();
+
+            using (var db = new GeradorInstaladoresContext())
+            {
+                int definicoes = (from c in db.DefinicoesGerais
+                                  select c).Count();
+
+                Assert.IsTrue(definicoes > 0);
+            }
+        }
+    }
+}
